eb7e22c5eacea45b71c7f73522e59271ffe57b0a
Hugo Souza <hugo.souza0610@gmail.com>
2023-05-26 23:51:45 -0400

    Traduz os comandos para portugues

--- src/help.cpp
+++ src/help.cpp
@@ -1,21 +1,37 @@
 #include "help.h"
+#include "interface.h"
 
 #include <stdio.h>
 
 void exibir_ajuda(){
-    printf("d - Exibe o tabuleiro.\n");	
-    printf("f - Inverte o tabuleiro.\n");
-    printf("go - A engine começa a pensar.\n");
-    printf("help - Exibe uma lista de comandos.\n");					
-    printf("moves - Exibe todos os lances possíveis na posição.\n");
-    printf("new - Começa um novo jogo.\n");
-    printf("on - Liga a engine.\n");
-    printf("off - Desliga a engine.\n");
-    printf("sb - Carrega uma posição a partir do FEN.\n");
-    printf("sd - Configura o limite de profundidade da pesquisa.\n");
-    printf("st - Configura o tempo de pesquisa da engine.\n");
-    printf("sw - Muda os lados dos jogadores.\n");
-    printf("quit - Encerra o programa.\n");
-    printf("undo - Desfaz o último lance.\n");
-    printf("xboard - Inicia xboard.\n");	
+    printf(COMANDO_DISPLAY_TABULEIRO);
+    printf(" - Exibe o tabuleiro.\n");	
+    printf(COMANDO_INVERTER_TABULEIRO);
+    printf(" - Inverte o tabuleiro.\n");
+    printf(COMANDO_COMPUTADOR_CALCULAR);
+    printf(" - A engine começa a pensar.\n");
+    printf(COMANDO_EXIBIR_AJUDA);
+    printf(" - Exibe uma lista de comandos.\n");		
+    printf(COMANDO_EXIBIR_LANCES);			
+    printf(" - Exibe todos os lances possíveis na posição.\n");
+    printf(COMANDO_NOVA_PARTIDA);
+    printf(" - Começa um novo jogo.\n");
+    printf(COMANDO_ATIVAR_ENGINE);
+    printf(" - Liga a engine.\n");
+    printf(COMANDO_DESATIVAR_ENGINE);
+    printf(" - Desliga a engine.\n");
+    printf(COMANDO_CARREGAR_FEN);
+    printf(" - Carrega uma posição a partir do FEN.\n");
+    printf(COMANDO_CONFIGURAR_PROFUNDIDADE);
+    printf(" - Configura o limite de profundidade da pesquisa.\n");
+    printf(COMANDO_CONFIGURAR_TEMPO);
+    printf(" - Configura o tempo de pesquisa da engine.\n");
+    printf(COMANDO_TROCAR_DE_LADO);
+    printf(" - Muda os lados dos jogadores.\n");
+    printf(COMANDO_ENCERRAR);
+    printf(" - Encerra o programa.\n");
+    printf(COMANDO_DESFAZER_LANCE);
+    printf(" - Desfaz o último lance.\n");
+    printf(COMANDO_INICIA_XBOARD);
+    printf(" - Inicia xboard.\n");	
 }
