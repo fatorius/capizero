a6ccc06bff904932a61149503ad79728ebea62a3
Hugo Souza <hugo.souza0610@gmail.com>
2023-11-26 04:45:41 -0400

    Simplificado o código das aberturas

--- tests/aberturas.py
+++ tests/aberturas.py
@@ -2,92 +2,31 @@ import chess
 import chess.pgn
 from random import randint
 
+aberturas = [
+    ["e2e4", "e7e5"],
+    ["e2e4", "c7c5"],
+    ["e2e4", "e7e6"],
+    ["e2e4", "c7c6"],
+    ["e2e4", "g7g6"],
+    ["d2d4", "d7d5"],
+    ["d2d4", "g8f6"],
+    ["c2c4", "c7c5"],
+    ["c2c4", "g8f6"],
+    ["c2c4", "e7e5"]
+]
 
 def escolher_abertura_aleatoria(pgn):
-    n = randint(0, 9)
-
     tabuleiro = chess.Board()
 
-    if n == 0:
-        node = pgn.add_variation(chess.Move.from_uci("e2e4"))
-        tabuleiro.push(chess.Move.from_uci("e2e4"))
+    n = randint(0, len(aberturas))
 
-        node = node.add_variation(chess.Move.from_uci("e7e5"))
-        tabuleiro.push(chess.Move.from_uci("e7e5"))
+    abertura_escolhida = aberturas[n]
 
-        return [node, tabuleiro]
-    elif n == 1:
-        node = pgn.add_variation(chess.Move.from_uci("e2e4"))
-        tabuleiro.push(chess.Move.from_uci("e2e4"))
+    for lance in abertura_escolhida:
+            node = pgn.add_variation(chess.Move.from_uci(lance))
+            tabuleiro.push(chess.Move.from_uci(lance))
 
-        node = node.add_variation(chess.Move.from_uci("c7c5"))
-        tabuleiro.push(chess.Move.from_uci("c7c5"))
-
-        return [node, tabuleiro]
-    elif n == 2:
-        node = pgn.add_variation(chess.Move.from_uci("e2e4"))
-        tabuleiro.push(chess.Move.from_uci("e2e4"))
-
-        node = node.add_variation(chess.Move.from_uci("e7e6"))
-        tabuleiro.push(chess.Move.from_uci("e7e6"))
-
-        return [node, tabuleiro]
-    elif n == 3:
-        node = pgn.add_variation(chess.Move.from_uci("e2e4"))
-        tabuleiro.push(chess.Move.from_uci("e2e4"))
-
-        node = node.add_variation(chess.Move.from_uci("c7c6"))
-        tabuleiro.push(chess.Move.from_uci("c7c6"))
-
-        return [node, tabuleiro]
-    elif n == 4:
-        node = pgn.add_variation(chess.Move.from_uci("e2e4"))
-        tabuleiro.push(chess.Move.from_uci("e2e4"))
-
-        node = node.add_variation(chess.Move.from_uci("g7g6"))
-        tabuleiro.push(chess.Move.from_uci("g7g6"))
-
-        return [node, tabuleiro]
-    elif n == 5:
-        node = pgn.add_variation(chess.Move.from_uci("d2d4"))
-        tabuleiro.push(chess.Move.from_uci("d2d4"))
-
-        node = node.add_variation(chess.Move.from_uci("d7d5"))
-        tabuleiro.push(chess.Move.from_uci("d7d5"))
-
-        return [node, tabuleiro]
-    elif n == 6:
-        node = pgn.add_variation(chess.Move.from_uci("d2d4"))
-        tabuleiro.push(chess.Move.from_uci("d2d4"))
-
-        node = node.add_variation(chess.Move.from_uci("g8f6"))
-        tabuleiro.push(chess.Move.from_uci("g8f6"))
-
-        return [node, tabuleiro]
-    elif n == 7:
-        node = pgn.add_variation(chess.Move.from_uci("c2c4"))
-        tabuleiro.push(chess.Move.from_uci("c2c4"))
-
-        node = node.add_variation(chess.Move.from_uci("c7c5"))
-        tabuleiro.push(chess.Move.from_uci("c7c5"))
-
-        return [node, tabuleiro]
-    elif n == 8:
-        node = pgn.add_variation(chess.Move.from_uci("c2c4"))
-        tabuleiro.push(chess.Move.from_uci("c2c4"))
-
-        node = node.add_variation(chess.Move.from_uci("g8f6"))
-        tabuleiro.push(chess.Move.from_uci("g8f6"))
-
-        return [node, tabuleiro]
-    else:
-        node = pgn.add_variation(chess.Move.from_uci("c2c4"))
-        tabuleiro.push(chess.Move.from_uci("c2c4"))
-
-        node = node.add_variation(chess.Move.from_uci("e7e5"))
-        tabuleiro.push(chess.Move.from_uci("e7e5"))
-
-        return [node, tabuleiro]
+    return [node, tabuleiro]
 
 
 
